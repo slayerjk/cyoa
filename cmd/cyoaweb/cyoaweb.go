88c8c93a96ae9b67dcad644fb9f98d8e5d10945b
slayerjk <slayer.jk@mail.ru>
2024-09-19 17:10:14 +0500

    refactor

--- cmd/cyoaweb/cyoaweb.go
+++ cmd/cyoaweb/cyoaweb.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"cyoa/internal/story"
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
-
-	"cyoa/story"
 )
+
+const htmlTemplatePath = "template.html"
 
 func main() {
 	// using flag for file
-	storyFile := flag.String("f", "../../gopher.json", "the JSON file with CYOA story")
+	storyFile := flag.String("f", "gopher.json", "the JSON file with CYOA story")
+	serverPort := flag.Int("p", 3000, "the port to start the server on")
 	flag.Parse()
 
 	storyParsed, err := story.ParseStoryJson(*storyFile)
@@ -19,4 +22,10 @@ func main() {
 	}
 
 	fmt.Printf("%+v\n", storyParsed)
+
+	templ, err := template.ParseFiles("template.html")
+	if err != nil {
+		log.Fatalf("failed to parse the template file %s:\n\t%v", htmlTemplatePath, err)
+	}
+	fmt.Println(templ)
 }
